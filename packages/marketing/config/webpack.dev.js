66e754cf4761fe2e03841e15689cb1314198795c
Pretorius <martin.pretorius@accenture.com>
2024-06-04 08:11:26 +0200

    First commit from work laptop

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,32 @@
+// import { merge } from "webpack-merge"; // merge is a function to merge together 2 webpack config objects
+// import HtmlWebpackPlugin from "html-webpack-plugin"; // inject script tags inside a html file
+// import commonConfig from "./webpack.common.js";
+
+const { merge } = require("webpack-merge"); // merge is a function to merge together 2 webpack config objects
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // inject script tags inside a html file
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin.js");
+const commonConfig = require("./webpack.common.js");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Marketing": "./src/bootstrap",
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig); // by listing devConfig 2nd it will override similar options inside commonConfig
