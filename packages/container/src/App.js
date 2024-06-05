e607b58b8f802bcb98cb74300dfb7356ef30f993
Martin <martinsbox@gmail.com>
2024-06-05 14:45:23 +0200

    change default region

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hello Micro Front End & React world!!!</h1>
+      <h1>Hello Micro Front End & React world!</h1>
       <hr />
       <MarketingApp />
     </div>
