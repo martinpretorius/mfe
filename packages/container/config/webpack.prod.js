d1f961ce2f71e2353f6c41f22773f9e9d158c175
Martin <martinsbox@gmail.com>
2024-06-07 16:05:26 +0200

    add marketing workflow

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,7 +3,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
-const domain = process.env.PRODUCTION_DOMAIN; // environment variable that gets defined when we build our application through our CI/CD pipeline, it contains a string that says where our production app is hosted
+const domain = process.env.PRODUCTION_DOMAIN; // environment variable that gets defined when we build our application with webpack through our CI/CD pipeline, it contains a string that says where our production app is hosted
 
 const prodConfig = {
   mode: "production", // this instructs webpack to optimize and minify the js files during build(webpack takes a little longer to load)
@@ -16,7 +16,7 @@ const prodConfig = {
       name: "container", // not required to add the name for container but recommended by docs
       remotes: {
         // where we should go to get some source code
-        marketingApp: `marketing@${domain}/marketing/remoteEntry.js`, // this key matches up to some import somewhere inside our container project, domain gets set when we create our infrastructure on amazon s3
+        marketingApp: `marketing@${domain}/marketing/latest/remoteEntry.js`, // this key matches up to some import somewhere inside our container project, domain gets set when we create our infrastructure on amazon s3; whenever we want to find marketing then go to the cloudfront-domain/marketing/latest/remoteEntry.js and that will tell us how to load the code for our marketing app
       },
       shared: packageJson.dependencies,
     }),
