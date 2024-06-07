f432b4881e44afe4194b5166c00cdd402c78c923
Martin <martinsbox@gmail.com>
2024-06-07 23:04:17 +0200

    dsdfsd

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import {
-  StylesProvider,
-  createGenerateClassName,
-} from "@material-ui/core/styles";
+// import {
+//   StylesProvider,
+//   createGenerateClassName,
+// } from "@material-ui/core/styles";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
 // generate a unique prefix for all css class names in this project to avoid class name collisions with other projects when ever you use a css-in-js library
-const generateClassName = createGenerateClassName({
-  productionPrefix: "co",
-});
+// const generateClassName = createGenerateClassName({
+//   productionPrefix: "co",
+// });
 
 export default () => {
   return (
     <BrowserRouter>
-      <StylesProvider generateClassName={generateClassName}>
-        <div>
-          <Header />
-          <MarketingApp />
-        </div>
-      </StylesProvider>
+      {/* <StylesProvider generateClassName={generateClassName}> */}
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+      {/* s</StylesProvider> */}
     </BrowserRouter>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -16,14 +16,12 @@ export default () => {
   return (
     // StylesProvider is a react comp used to customize all the css-in-js generation stuff
     <StylesProvider generateClassName={generateClassName}>
-      <div>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route path="/" component={Landing} />
-          </Switch>
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/pricing" component={Pricing} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </BrowserRouter>
     </StylesProvider>
   );
 };
