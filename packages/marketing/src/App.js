2d8d9c1597f9e64632c14fbb3152b7c388c94e32
Martin <martinsbox@gmail.com>
2024-06-08 10:52:12 +0200

    ouiouio

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,28 +1,24 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-// import {
-//   StylesProvider,
-//   createGenerateClassName,
-// } from "@material-ui/core/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
 import Landing from "./components/Landing.js";
 import Pricing from "./components/Pricing.js";
 
 // generate a unique prefix for all css class names in this project to avoid class name collisions with other projects when ever you use a css-in-js library
-// const generateClassName = createGenerateClassName({
-//   productionPrefix: "ma-",
-// });
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma-",
+});
 
 export default () => {
   return (
     // StylesProvider is a react comp used to customize all the css-in-js generation stuff
-
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/pricing" component={Pricing} />
-        {/* <StylesProvider generateClassName={generateClassName}> */}
-        <Route path="/" component={Landing} />
-        {/* </StylesProvider> */}
-      </Switch>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/pricing" component={Pricing} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
