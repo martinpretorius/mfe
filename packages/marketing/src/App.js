f5de175277a6f9a8134897bd159037e2f6fb5f4b
Martin <martinsbox@gmail.com>
2024-06-08 11:24:58 +0200

    ertert

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -16,7 +16,7 @@ const generateClassName = createGenerateClassName({
 
 export default () => {
   return (
-    // StylesProvider is a react comp used to customize all the css-in-js generation stuff
+    // StylesProvider is a react comp used to customize all the css-in-js generation stuff.
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
